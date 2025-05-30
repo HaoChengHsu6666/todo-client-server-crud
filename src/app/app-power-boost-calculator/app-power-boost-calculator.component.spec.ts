af5779cb43320a2f4a59a866da0ac5c68a711965
ChrisHsu <becomerocker@gmail.com>
2025-05-30 20:06:12 +0800

    feat: add-power-boost-calculator

---
+++ src/app/app-power-boost-calculator/app-power-boost-calculator.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AppPowerBoostCalculatorComponent } from './app-power-boost-calculator.component';
+
+describe('AppPowerBoostCalculatorComponent', () => {
+  let component: AppPowerBoostCalculatorComponent;
+  let fixture: ComponentFixture<AppPowerBoostCalculatorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AppPowerBoostCalculatorComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(AppPowerBoostCalculatorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppPowerBoostCalculatorComponent } from './app-power-boost-calculator/app-power-boost-calculator.component';
+import { ExponentialStrengthPipePipe } from './exponential-strength-pipe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppPowerBoostCalculatorComponent,
+    ExponentialStrengthPipePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

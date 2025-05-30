c361799a643e171c8813fd784cd7ebd7ca0aa39d
ChrisHsu <becomerocker@gmail.com>
2025-05-30 17:11:25 +0800

    MRT_List_test

---
+++ src/app/station-list-const.ts
@@ -0,0 +1,43 @@
+export const stationList = [
+
+    {
+      "Station": "三重國小站",
+      "Destination": "南勢角站",
+      "UpdateTime": "2018-10-15T22:14:41.557"
+    },
+    {
+      "Station": "土林站",
+      "Destination": "大安站",
+      "UpdateTime": "2018-10-15T22:14:35.497"
+    },
+    {
+      "Station": "土林站",
+      "Destination": "淡水站",
+      "UpdateTime": "2018-10-15T22:14:25.34"
+    },
+    {
+      "Station": "大安森林公園站",
+      "Destination": "大安站",
+      "UpdateTime": "2018-10-15T22:14:25.34"
+    },
+    {
+      "Station": "大坪林站",
+      "Destination": "松山站",
+      "UpdateTime": "2018-10-15T22:14:52.737"
+    },
+    {
+      "Station": "大直站",
+      "Destination": "動物園站",
+      "UpdateTime": "2018-10-15T22:14:41"
+    },
+    {
+      "Station": "中山站",
+      "Destination": "新店站",
+      "UpdateTime": "2018-10-15T22:14:52.737"
+    },
+    {
+      "Station": "中山國中站",
+      "Destination": "南港展覽館站",
+      "UpdateTime": "2018-10-15T22:14:41"
+    }
+]

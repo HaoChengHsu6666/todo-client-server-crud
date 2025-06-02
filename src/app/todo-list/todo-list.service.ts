fda2e983e57f968f1b02daa52f72c01cb15b5f54
ChrisHsu <becomerocker@gmail.com>
2025-06-02 16:09:08 +0800

    feat: add todo-list_create&update&filter

--- src/app/todo-list/todo-list.service.ts
+++ src/app/todo-list/todo-list.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Todo } from './todo.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoListService {
 
-  private list: string[] = [];
+  private list: Todo [] = [];
 
   constructor() { }
 
@@ -19,7 +20,7 @@ export class TodoListService {
 
     // 避免傳入的 title 是無效值或空白字串，稍微判斷一下
     if (title || title.trim()) {
-      this.list.push(title);
+      this.list.push(new Todo(title));
     }
 
   }
@@ -30,8 +31,37 @@ export class TodoListService {
    * @returns {string[]}
    * @memberof TodoListService
    */
-  getList(): string[] {
+  getList(): Todo[] {
     return this.list;
   }
 
+  /**
+   * 移除待辦事項
+   *
+   * @param {number} index - 待辦事項的索引位置
+   * @memberof TodoListService
+   */
+  remove(index: number): void {
+    this.list.splice(index, 1);
+  }
+
+  /**
+   * 取得已完成/未完成的清單
+   *
+   * @param {boolean} completed - 要取得已完成還是未完成的清單
+   * @returns {Todo[]}
+   * @memberof TodoListService
+   */
+  getWithCompleted(completed: boolean): Todo[] {
+    return this.list.filter(todo => todo.done === completed);
+  }
+
+  /**
+   * 從清單中移除所有已完成之待辦事項
+   *
+   * @memberof TodoListService
+   */
+  removeCompleted(): void {
+    this.list = this.getWithCompleted(false);
+  }
 }
